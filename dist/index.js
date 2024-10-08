b1b85383dddb8101538a4f70e3f1aae7efda6707
Federico Silva Armas <farmas@uw.edu>
2024-10-08 14:34:53 -0700

    WIP.

--- dist/index.js
+++ dist/index.js
@@ -1632,7 +1632,7 @@ class MSTeams {
     const client = new IncomingWebhook(url);
     const response = await client.send(payload);
 
-    if (!response.text) {
+    if (response.text !== "") {
       throw new Error('Failed to send notification to Microsoft Teams.\n' + 'Response:\n' + JSON.stringify(response, null, 2));
     }
   }
